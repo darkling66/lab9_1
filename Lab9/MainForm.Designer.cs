b95d8b0383c6ad96443878077b8d790909412551
darkling66 <darkling6667@gmail.com>
2018-12-06 23:50:42 +0200

    Init commit

---
+++ Lab9/MainForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Lab9
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx1min = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbx2min = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbx2max = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbx1max = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbdx2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbdx1 = new System.Windows.Forms.TextBox();
+            this.gv = new System.Windows.Forms.DataGridView();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tbSum = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbx1min
+            // 
+            this.tbx1min.Location = new System.Drawing.Point(72, 12);
+            this.tbx1min.Name = "tbx1min";
+            this.tbx1min.Size = new System.Drawing.Size(73, 20);
+            this.tbx1min.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X1min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X2min";
+            // 
+            // tbx2min
+            // 
+            this.tbx2min.Location = new System.Drawing.Point(72, 49);
+            this.tbx2min.Name = "tbx2min";
+            this.tbx2min.Size = new System.Drawing.Size(73, 20);
+            this.tbx2min.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X2max";
+            // 
+            // tbx2max
+            // 
+            this.tbx2max.Location = new System.Drawing.Point(243, 49);
+            this.tbx2max.Name = "tbx2max";
+            this.tbx2max.Size = new System.Drawing.Size(73, 20);
+            this.tbx2max.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "X1max";
+            // 
+            // tbx1max
+            // 
+            this.tbx1max.Location = new System.Drawing.Point(243, 12);
+            this.tbx1max.Name = "tbx1max";
+            this.tbx1max.Size = new System.Drawing.Size(73, 20);
+            this.tbx1max.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(357, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "dX2";
+            // 
+            // tbdx2
+            // 
+            this.tbdx2.Location = new System.Drawing.Point(417, 49);
+            this.tbdx2.Name = "tbdx2";
+            this.tbdx2.Size = new System.Drawing.Size(73, 20);
+            this.tbdx2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(357, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "dX1";
+            // 
+            // tbdx1
+            // 
+            this.tbdx1.Location = new System.Drawing.Point(417, 12);
+            this.tbdx1.Name = "tbdx1";
+            this.tbdx1.Size = new System.Drawing.Size(73, 20);
+            this.tbdx1.TabIndex = 8;
+            // 
+            // gv
+            // 
+            this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv.Location = new System.Drawing.Point(15, 86);
+            this.gv.Name = "gv";
+            this.gv.Size = new System.Drawing.Size(475, 262);
+            this.gv.TabIndex = 12;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(508, 144);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(94, 25);
+            this.btnCalc.TabIndex = 13;
+            this.btnCalc.Text = "Розрахувати";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(508, 184);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 25);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Очистити";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(508, 323);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 25);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Вийти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tbSum
+            // 
+            this.tbSum.Location = new System.Drawing.Point(508, 108);
+            this.tbSum.Name = "tbSum";
+            this.tbSum.Size = new System.Drawing.Size(94, 20);
+            this.tbSum.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(496, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Сума від\'ємних значень";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 360);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbSum);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.gv);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbdx2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbdx1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbx2max);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbx1max);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx2min);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbx1min);
+            this.Name = "MainForm";
+            this.Text = "Лабораторна робота №9";
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx1min;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbx2min;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbx2max;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbx1max;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbdx2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbdx1;
+        private System.Windows.Forms.DataGridView gv;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox tbSum;
+        private System.Windows.Forms.Label label7;
+    }
+}
